659491951c7a571aa9ead040d3a5719e3e165c23
laopanmemz <pr86139083@126.com>
2025-05-19 15:36:52 +0800

    更新支持WechatPadPro消息平台。

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import aiohttp
 from astrbot.api import logger
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
-from astrbot.core.platform.sources.gewechat.gewechat_event import GewechatPlatformEvent
+from astrbot.core.platform.sources.wechatpadpro.wechatpadpro_message_event import WeChatPadProMessageEvent
 
-@register("astrbot_plugin_membercontrast", "laopanmemz", "微信群组群员历史对比", "1.0.0")
+
+@register("astrbot_plugin_membercontrast", "laopanmemz", "微信群组群员历史对比", "1.1.0")
 class Watcher(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -14,28 +15,27 @@ class Watcher(Star):
 
     # 构造请求群员列表方法
     async def post_members(self, event: AstrMessageEvent):
-        assert isinstance(event, GewechatPlatformEvent)
+        assert isinstance(event, WeChatPadProMessageEvent)
         try:
             payloads = {
-                "appId": event.client.appid,
-                "chatroomId": event.get_group_id(),
+                "ChatRoomWxIdList": [event.get_group_id()]
             }
             headers = {
-                "X-GEWE-TOKEN": event.client.token,
+                "accept": "application/json",
                 "Content-Type": "application/json"
             }
-            gewe_url = event.client.base_url + "/group/getChatroomMemberList"
-
+            params = {"key": event.adapter.auth_key}
+            wxapi_url = event.adapter.base_url + "/group/GetChatRoomInfo"
             async with aiohttp.ClientSession() as session:
-                async with session.post(gewe_url, headers=headers, json=payloads) as resp:
+                async with session.post(wxapi_url, headers=headers, json=payloads, params=params) as resp:
                     if resp.status != 200:
                         logger.error(f"❌获取成员列表失败，状态码: {resp.status}")
                         return []
                     data = await resp.json()
                     logger.info(data)
-                    members = data.get("data", {}).get("memberList", [])
+                    members = data['Data']['contactList'][0]['newChatroomData']['chatroom_member_list']
                     logger.info(members)
-                    return {m["wxid"]: m["nickName"] for m in members}
+                    return {m["user_name"]: m["nick_name"] for m in members}
 
         except Exception as e:
             logger.error(f"❌获取成员列表失败: {e}")
@@ -44,7 +44,7 @@ class Watcher(Star):
     # 加载缓存群员列表，如果缓存为空则使用请求方法获取列表
     async def load_members(self,event: AstrMessageEvent):
         # 创建缓存目录（如果不存在）
-        cache_dir = os.path.join("data", "plugins", "astrbot_plugin_membercontrast", "member-cache")
+        cache_dir = os.path.join("data", "member-cache")
         os.makedirs(cache_dir, exist_ok=True)
 
         # 获取当前群组ID
@@ -71,12 +71,12 @@ class Watcher(Star):
         return self.member_cache
 
     # 注册指令的装饰器。指令名为 对比成员。注册成功后，发送 `/对比成员` 就会触发这个指令`
-    @filter.command("对比成员")
+    @filter.command("对比成员",alias={"成员对比", "检查成员", "群员对比", "对比群员", "差异对比", "对比群成员"})
     async def start(self, event: AstrMessageEvent):
-        cache_dir = os.path.join("data", "plugins", "astrbot_plugin_membercontrast", "member-cache")
+        cache_dir = os.path.join("data", "member-cache")
         cache_file = os.path.join(cache_dir, f"member_cache_{event.get_group_id()}.json")
 
-        if event.get_platform_name() == "gewechat": # 判断是否为微信
+        if event.get_platform_name() == "wechatpadpro": # 判断是否为微信
             last_member = await self.load_members(event) # 获取缓存的成员列表
             member_data = await self.post_members(event) # 获取最新的成员列表
             if member_data == last_member: # 如果缓存的成员列表和最新的成员列表相同，则发送提示消息
@@ -101,10 +101,10 @@ class Watcher(Star):
                         logger.info("✅成功更新缓存成员列表！")
 
         if event.get_platform_name() == "aiocqhttp":
-            yield event.plain_result("\n⚠检测功能仅支持微信......\nQQ都有 退群通知 和 Q群管家入群欢迎功能 了，你还要这个干啥？（（大雾")
+            yield event.plain_result("\n⚠检测功能仅支持微信端（WechatPadPro）......\nQQ都有 退群通知 和 Q群管家入群欢迎功能 了，你还要这个干啥？（（大雾")
             event.stop_event()
             return
-        if event.get_platform_name() != "gewechat" and event.get_platform_name() != "aiocqhttp":
-            yield event.plain_result("\n⚠检测功能仅支持微信......")
+        if event.get_platform_name() != "wechatpadpro" and event.get_platform_name() != "aiocqhttp":
+            yield event.plain_result("\n⚠检测功能仅支持微信端（WechatPadPro）。")
             event.stop_event()
             return 
